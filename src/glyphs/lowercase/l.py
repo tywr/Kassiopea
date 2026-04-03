b87b217a65f2e1b1cf9e16fc9e084d841097885d
tywr <tanguy.viv@gmail.com>
2026-04-03 23:21:15 +0200

    refactor: l

--- src/glyphs/lowercase/l.py
+++ src/glyphs/lowercase/l.py
@@ -1,4 +1,3 @@
-from config import FontConfig as fc
 from glyph import Glyph
 from shapes.rect import draw_rect
 
@@ -6,33 +5,17 @@ from shapes.rect import draw_rect
 class LowercaseLGlyph(Glyph):
     name = "lowercase_l"
     unicode = "0x6C"
+    offset = 20
+    rl_ratio = 0.5
 
-    def draw(
-        self,
-        pen,
-        stroke: int,
-    ):
-        offset = 20
-        len_left = 200
-        len_right = 200
-        len_cap = 200
+    def draw(self, pen, dc):
+        b = dc.body_bounds(offset=self.offset, height="ascent")
+        right_len = b.width * self.rl_ratio - dc.stroke / 2
+        left_len = b.width * (1 - self.rl_ratio) - dc.stroke / 2
 
-        xmid = fc.width / 2 + offset
         # Stem
-        draw_rect(pen, xmid - stroke / 2, 0, xmid + stroke / 2, fc.ascent)
+        draw_rect(pen, b.xmid - dc.stroke / 2, 0, b.xmid + dc.stroke / 2, dc.ascent)
         # Footer
-        draw_rect(
-            pen,
-            xmid - len_left - stroke / 2,
-            0,
-            xmid + len_right + stroke / 2,
-            stroke
-        )
+        draw_rect(pen, b.xmid - left_len - dc.stroke / 2, 0, b.xmid + right_len + dc.stroke / 2, dc.stroke)
         # Left cap
-        draw_rect(
-            pen,
-            xmid - len_cap - stroke / 2,
-            fc.ascent - stroke,
-            xmid,
-            fc.ascent,
-        )
+        draw_rect(pen, b.xmid - left_len - dc.stroke / 2, dc.ascent - dc.stroke, b.xmid, dc.ascent)
