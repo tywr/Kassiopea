c581126c5372530306e452d18ba637160cdb6cae
tywr <tanguy.viv@gmail.com>
2026-04-14 18:12:35 +0200

    adjust: stroke

--- src/config.py
+++ src/config.py
@@ -35,8 +35,8 @@ class FontConfig:
 class DrawConfig(FontConfig):
     # Default parameters
     stroke_x: int = 90
-    stroke_y: int = 82
-    stroke_alt: int = 72
+    stroke_y: int = 80
+    stroke_alt: int = 70
     width: int = 340
 
     hx: int = 172
